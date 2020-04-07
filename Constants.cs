5c977a3130305dfe7d758cb98dc7033ead087101
john-aldridge <5142642+john-aldridge@users.noreply.github.com>
2020-04-06 18:53:15 -0600

    Make dashboard embed values the default

--- Constants.cs
+++ Constants.cs
@@ -5,7 +5,7 @@
         private static readonly string ApiHost = "https://api.domo.com";
         private static readonly string EmbedHost = "https://public.domo.com";
         public static readonly string AccessTokenUrl = ApiHost + "/oauth/token?grant_type=client_credentials&scope=data%20audit%20user%20dashboard";
-        public static readonly string EmbedTokenUrl = ApiHost + "/v1/cards/embed/auth";
-        public static readonly string EmbedUrl = EmbedHost + "/cards/";
+        public static readonly string EmbedTokenUrl = ApiHost + "/v1/stories/embed/auth";
+        public static readonly string EmbedUrl = EmbedHost + "/embed/pages/";
     }
 }
